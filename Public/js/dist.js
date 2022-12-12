c63878a93ca5e0904868413c0b6cf2c177529552
Malo Chaperon <chaperon.malo@gmail.com>
2022-12-12 23:05:05 +0100

    all

--- Public/js/dist.js
+++ Public/js/dist.js
@@ -1,6 +1,53 @@
+// scroll reveal anime
+const sr = ScrollReveal();
+sr.reveal('#navRubrique', {
+delay: 200,
+duration: 1300,
+distance: "100px",
+origin: "bottom"
+});
+
+sr.reveal('#intro_logo', {
+delay: 200,
+duration: 1300,
+distance: "100px",
+origin: "bottom"
+});
+
+sr.reveal('#arme_logo', {
+delay: 300,
+duration: 1300,
+distance: "100px",
+origin: "bottom"
+});
+
+sr.reveal('.button-introduction', {
+delay: 300,
+duration: 1300,
+distance: "100px",
+origin: "bottom"
+});
+
+sr.reveal('#intro_title', {
+delay: 300,
+duration: 1300,
+distance: "100px",
+origin: "bottom"
+});			
+
+
 var navOpen = document.getElementById("navOpen");
+var burgerNav = document.getElementById("burgerIcon");
+
 function clickOpenMenu(){
-   navOpen.classList.add("displayActive");
+   navOpen.classList.toggle("navOpen");
+   burgerNav.classList.toggle("burgerClose");
+
+   if (burgerNav.innerHTML === "Menu") {
+      burgerNav.innerHTML = "Close";
+  } else {
+   burgerNav.innerHTML = "Menu";
+  }
 }
 
 
